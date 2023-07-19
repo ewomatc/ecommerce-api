68e2e187be7f8055ecb0ab57b16f09a80095926a
Great Diro <iamgreatdiro@gmail.com>
2023-07-19 21:33:32 +0100

    added all endpoints for products router and updated all models

--- models/user.js
+++ models/user.js
@@ -6,4 +6,12 @@ const userSchema = new mongoose.Schema({
   password: String
 })
 
+userSchema.virtual('id').get(function () {
+  return this._id.toHexString()
+})
+
+userSchema.set('toJSON', {
+  virtuals: true
+})
+
 exports.User = mongoose.model('User', userSchema)
--- routers/productRouter.js
+++ routers/productRouter.js
@@ -1,16 +1,25 @@
 const {Product} = require('../models/product')
 const {Category} = require('../models/category')
 const router = require('express').Router()
+const mongoose = require('mongoose')
 
 
 //get products list
 router.get(`/`, async(req, res) => {
+  //filter products using req.query based on query parameters from url
+  
+    //if a categories query is passed in the url, then we'll split it and pass it to the Product.find(). else the value passed in find() will be an empty object.
   try {
-    const productList = await Product.find().populate('category')
+    const {categories} = req.query;
+
+    const filter = categories ? { category : categories.split(',')} : {}
+    const productList = await Product.find(filter).populate('category')
     res.status(200).json(productList)
   }
   catch(err){
-    res.json(err)
+    res.status(500).json({
+      error: err
+    })
   }
 })
 
@@ -60,6 +69,10 @@ router.post(`/`, async(req, res) => {
 
 //get a single product by it's id
 router.get('/:id', async (req, res) => {
+
+  if (!mongoose.isValidObjectId(req.params.id)) {
+    return res.status(400).json({ error: 'Invalid id'})
+  }
   try {
     const product = await Product.findById(req.params.id).populate('category')
 
@@ -80,7 +93,11 @@ router.get('/:id', async (req, res) => {
 //update a product
 router.put('/:id', async (req, res) => {
 
-  const body = req.body
+  if (!mongoose.isValidObjectId(req.params.id)) {
+    return res.status(400).json({
+      error: 'Invalid id'
+    })
+  }
 
   const category = await Category.findById(req.body.category)
   if(!category) {
@@ -88,6 +105,8 @@ router.put('/:id', async (req, res) => {
       error: 'Category does not exist'
     })
   }
+
+  const body = req.body
 
   try {
     const product = await Product.findByIdAndUpdate(req.params.id, {
@@ -110,8 +129,8 @@ router.put('/:id', async (req, res) => {
     if (product) {
       res.json(product)
     } else {
-      res.status(400).json({
-        error: 'could not update product'
+      res.status(404).json({
+        error: 'Product not found'
       })
     }
   } catch (error) {
@@ -120,6 +139,82 @@ router.put('/:id', async (req, res) => {
     })
   }
 })
+
+//delete a product
+router.delete('/:id', async (req, res) => {
+
+  if (!mongoose.isValidObjectId(req.params.id)) {
+    return res.status(400).json({
+      error: 'Invalid id'
+    })
+  }
+
+  try {
+    const productDeleted = await Product.findByIdAndDelete(req.params.id)
+
+    if (productDeleted) {
+      res.status(204).json({
+        success: true,
+        message: 'Product deleted successfully'
+      })
+    } else {
+      res.status(404).json({
+        error: 'Product with this id does not exist'
+      })
+    }
+  }
+  catch (error) {
+    res.status(500).json({
+      error: error
+    })
+  }
+})
+
+//get the total number of products available in the store
+router.get('/get/count', async (req, res) => {
+  try {
+    const productCount = await Product.countDocuments()
+
+    if (productCount) {
+      res.status(200).json({
+        totalProducts: productCount
+      })
+    } else {
+      res.status(404).json({
+        error: 'No product found'
+      })
+    }
+  } catch (error) {
+    res.status(500).json({
+      error: error
+    })
+  }
+})
+
+//get featured products
+router.get('/get/featured/:count', async (req, res) => {
+  const count = req.params.count ? req.params.count : 0
+
+  try {
+    const featuredProduct = await Product.find({ isFeatured: true }).limit(+count)
+
+    if (featuredProduct) {
+      res.status(200).json({
+        featuredProducts: featuredProduct
+      })
+    } else {
+      res.status(404).json({
+        error: `No product found`
+      })
+    }
+  }
+  catch (error) {
+    res.status(500).json({
+      error: error
+    })
+  }
+})
+
 
 
 module.exports = router
